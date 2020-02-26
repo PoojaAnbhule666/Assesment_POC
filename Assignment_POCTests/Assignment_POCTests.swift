d9436e61d9fb70649aad3cf405696e005dead29c
Pooja <anbhulepooja250@gmail.com>
2020-02-26 11:56:53 +0530

    Initial Commit

---
+++ Assignment_POCTests/Assignment_POCTests.swift
@@ -0,0 +1,34 @@
+//
+//  Assignment_POCTests.swift
+//  Assignment_POCTests
+//
+//  Created by Deep on 26/02/20.
+//  Copyright © 2020 Pooja. All rights reserved.
+//
+
+import XCTest
+@testable import Assignment_POC
+
+class Assignment_POCTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
